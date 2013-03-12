c5cc504ff2f6c56292d0d09bd578abdf272ba47a
Anton Yuzhaninov <ayuzhaninov@openstat.ru>
2013-03-12 09:28:23 +0000

    Add script useful for gmirror users (gmirrir is FreeBSD soft-RAID mirror).
    Autosynchronization of stale components (gmirror configure -a) is easy way
    to keep mirror synced after system reset or crash, but it has on big drawback:
    increased boot time because of disk contention between fsck and rebuild.
    This scripts allows to delay rebuild start for some time, enough to full system
    startup.

---
+++ gmirror-rebuild.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# to rebuild stale gmirror components after reboot
+# add to root cron:
+#
+# @reboot	sleep 7753; /usr/local/sbin/gmirror-rebuild.sh
+
+set -e
+
+PATH=/bin:/sbin:/usr/bin
+
+if [ `id -u` -ne 0 ]; then
+	echo 'You must be root to run this script'; echo;
+	exit 1
+fi
+
+if [ ! -d /dev/mirror ]; then
+	echo 'no gmirror found'
+	exit
+fi
+
+cd /dev/mirror/
+
+for mirror in *; do
+	gmirror status -s $mirror | awk '$4 == "(STALE)" {print $3}' | xargs gmirror rebuild $mirror
+	sleep 71
+done
