ded5c5e60415de5094a187e0d593b42cb5d86688
Anton Yuzhaninov <citrin@citrin.ru>
2013-04-19 16:02:55 +0400

    add jpeg2pdf.sh

---
+++ jpeg2pdf.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# portmaster graphics/ImageMagick print/ghostscript9
+
+DPI=300
+
+if [ ! -r "$2" ]; then
+        echo Usage: $0 merged.pdf page1.jpeg page2.jpeg ... pageN.jpeg
+        exit 64
+fi
+
+outfile=$1
+shift
+
+param=""
+toc=""
+n=1
+for f in "$@" ; do
+	size=$(identify -format "%[fx:round(72*w/${DPI})] %[fx:round(72*h/${DPI})]" "${f}")
+	param="$param <</PageSize [${size}]>> setpagedevice (${f}) viewJPEG showpage"
+	toc="$toc [/Page $n /Title (File $f) /OUT pdfmark"
+	n=$((n+1))
+done
+
+gs -sDEVICE=pdfwrite -dPDFSETTINGS=/prepress -dQUIET \
+	-o "$outfile" viewjpeg.ps -c "$toc" -c "$param"
+
+# viewjpeg.ps is located in /usr/local/share/ghostscript/9.??/lib/viewjpeg.ps
+# and this path should be in default search path (gs -h)
