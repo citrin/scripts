241f898825be69bd6a5ebcad92011276d19ad085
Anton Yuzhaninov <citrin+bb@citrin.ru>
2016-08-26 20:15:03 +0000

    Add comment why this script can be useful

--- lib_check.sh
+++ lib_check.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# find binaries with missing libs
+# they can appear after make -C /usr/src delete-old-libs
+
 find /usr/local/bin/ /usr/local/sbin/ /usr/local/libexec/ -type f -print0 \
 	| xargs -0 file --no-pad --separator ' ' \
 	| awk '/dynamically linked/ { print $1 }' \
