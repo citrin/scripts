7b7d0d2167e2ff6ed0dccb2fa518a9944c71f296
Anton Yuzhaninov <citrin+bb@citrin.ru>
2016-11-01 11:06:26 -0400

    Fix handling files with space in filename

--- split-image.sh
+++ split-image.sh
@@ -5,20 +5,20 @@ set -ex
 # emerge media-gfx/imagemagick
 # portmaster graphics/ImageMagick
 
-for file in $@; do
+for file in "$@"; do
 	NAME=${file%.*}
 	EXT=${file#$NAME.}
 
-	width=`identify -format %w $file`
+	width=`identify -format %w "${file}"`
 
 	# 100 is overlap between lists
-	new_width=$(($width / 2 + 100))
+	new_width=$(( $width / 2 + 100))
 
 	# A4 size 210x297 (portrait oriented)
 	new_height=$(( $new_width * 297 / 210 ))
 
-	convert -crop ${new_width}x${new_height}+0+0 -gravity NorthWest $NAME.$EXT $NAME-list1.png
-	convert -crop ${new_width}x${new_height}+0+0 -gravity NorthEast $NAME.$EXT $NAME-list2.png
-	convert -crop ${new_width}x${new_height}+0+0 -gravity SouthWest $NAME.$EXT $NAME-list3.png
-	convert -crop ${new_width}x${new_height}+0+0 -gravity SouthEast $NAME.$EXT $NAME-list4.png
+	convert -crop ${new_width}x${new_height}+0+0 -gravity NorthWest "${file}" "${NAME}-list1.png"
+	convert -crop ${new_width}x${new_height}+0+0 -gravity NorthEast "${file}" "${NAME}-list2.png"
+	convert -crop ${new_width}x${new_height}+0+0 -gravity SouthWest "${file}" "${NAME}-list3.png"
+	convert -crop ${new_width}x${new_height}+0+0 -gravity SouthEast "${file}" "${NAME}-list4.png"
 done
