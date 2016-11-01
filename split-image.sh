a691f9d2f1217c36faee8e462ecd0ba0676613de
Anton Yuzhaninov <citrin+bb@citrin.ru>
2016-11-01 11:08:42 -0400

    Style

--- split-image.sh
+++ split-image.sh
@@ -6,19 +6,18 @@ set -ex
 # portmaster graphics/ImageMagick
 
 for file in "$@"; do
-	NAME=${file%.*}
-	EXT=${file#$NAME.}
-
 	width=`identify -format %w "${file}"`
 
-	# 100 is overlap between lists
+	# 100 pixels is overlap between lists
 	new_width=$(( $width / 2 + 100))
 
 	# A4 size 210x297 (portrait oriented)
 	new_height=$(( $new_width * 297 / 210 ))
 
-	convert -crop ${new_width}x${new_height}+0+0 -gravity NorthWest "${file}" "${NAME}-list1.png"
-	convert -crop ${new_width}x${new_height}+0+0 -gravity NorthEast "${file}" "${NAME}-list2.png"
-	convert -crop ${new_width}x${new_height}+0+0 -gravity SouthWest "${file}" "${NAME}-list3.png"
-	convert -crop ${new_width}x${new_height}+0+0 -gravity SouthEast "${file}" "${NAME}-list4.png"
+	name=${file%.*}
+
+	convert -crop ${new_width}x${new_height}+0+0 -gravity NorthWest "${file}" "${name}-list1.png"
+	convert -crop ${new_width}x${new_height}+0+0 -gravity NorthEast "${file}" "${name}-list2.png"
+	convert -crop ${new_width}x${new_height}+0+0 -gravity SouthWest "${file}" "${name}-list3.png"
+	convert -crop ${new_width}x${new_height}+0+0 -gravity SouthEast "${file}" "${name}-list4.png"
 done
